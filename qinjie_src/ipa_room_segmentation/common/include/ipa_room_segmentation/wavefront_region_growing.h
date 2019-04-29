011d44f0f42dc4d30394b1e4912c6c6c23c74644
michaelyeah7 <michaelyeah7@126.com>
2019-04-29 16:21:03 -0500

    delete empty folders

--- qinjie_src/ipa_room_segmentation/common/include/ipa_room_segmentation/wavefront_region_growing.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <list>
-#include <vector>
-#include <math.h>
-#include <opencv2/opencv.hpp>
-#include <opencv2/highgui/highgui.hpp>
-
-void wavefrontRegionGrowing(cv::Mat& image);
